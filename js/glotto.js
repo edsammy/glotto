81c545f23013a5e2daedeec4caac68ab70816d45
edsammy <eddiemsamuels@gmail.com>
2015-03-24 15:28:03 -0400

    updated signature

--- js/glotto.js
+++ js/glotto.js
@@ -3,6 +3,6 @@ $( document ).ready(function() {
         e.preventDefault();
         var email = $('#email');
         var emailText = email.html();
-        email.hide().html('<span class="glotto" data-tooltip="Glotto recommends greetings!">Insert a greeting here.</span><br> <br> <span class="tooharsh" data-tooltip="Soften your tone.">'+emailText+'</span><br><br><br>--------------<br>This email has been filtered by <a href="">Glotto</a>').fadeIn(1000);
+        email.hide().html('<span class="glotto" data-tooltip="Glotto recommends greetings!">Insert a greeting here.</span><br> <br> <span class="tooharsh" data-tooltip="Soften your tone.">'+emailText+'</span><br><br><br>--------------<br>I <a href="">Glottoed</a> this message!').fadeIn(1000);
     });
 });
